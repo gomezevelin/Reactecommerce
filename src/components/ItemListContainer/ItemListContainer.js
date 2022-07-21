545a587acb9f1fd502e8e7636ebdc80323627b02
Evelin Gomez <gomez.evelin@hotmail.com>
2022-07-21 18:43:18 -0300

    count product

--- src/components/ItemListContainer/ItemListContainer.js
+++ src/components/ItemListContainer/ItemListContainer.js
@@ -2,12 +2,38 @@ import './ItemListContainer.scss'
 import ItemProduct from '../ItemProduct/ItemProduct'
 
 const ListContainer = ({section}) => {
+    const vela1={
+        title:"Vela Adela",
+        price:1500,
+        img:'/assets/vela1.jpg',
+        stock: 5, 
+    }
+    const vela2={
+        title:"Vela Mara",
+        price:1200,
+        img:'/assets/velaAlma.jpg',
+        stock: 3, 
+    }
+    const vela3={
+        title:"Vela Esperanza",
+        price:1700,
+        img:'/assets/velaEsperanza.jpg',
+        stock: 4, 
+    }
+    const vela4={
+        title:"Vela Milagros",
+        price:2000,
+        img:'/assets/velaMilagros.jpg',
+        stock: 2, 
+    }
     return(
         <div className='ContainerlistProducts'>
             <h1>{section}</h1>
             <div className='listProducts'>
-            <ItemProduct title="Vela Adela" price={1500} img={'/assets/vela1.jpg'} />
-            <ItemProduct title="Vela Mara" price={1200} img={'/assets/velaAlma.jpg'}/>
+            <ItemProduct  data={vela1}/>
+            <ItemProduct data={vela2}/>
+            <ItemProduct data={vela3}/>
+            <ItemProduct data={vela4}/>
             </div>
             
         </div>
--- src/components/ItemProduct/ItemProduct.js
+++ src/components/ItemProduct/ItemProduct.js
@@ -1,11 +1,25 @@
 import './ItemProduct.scss'
+import {useState} from 'react'
 
-const ItemProduct = ({title,price,img}) => {
+const ItemProduct = ({data}) => {
+    const [count, setCount] = useState(1)
+    const {title,price,img,stock} = data
+    const addNumber = () =>{
+        setCount(count+1)
+    }
+    const removeNumber = ()=>{
+        setCount(count-1)
+    }
     return(
         <div className='itemProduct'>
             <img className='imgProduct' src={img} alt="Imagen Producto"/>
             <p>{title}</p>
             <span>${price}</span>
+            <div className='itemCount'>
+                <button onClick={removeNumber}>-</button>
+                <p>{count}</p>
+                <button onClick={addNumber}>+</button>
+            </div>
             <button>Comprar</button>
         </div>
     )  
