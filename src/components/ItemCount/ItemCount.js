2896ddbf72ab23a6d545ec5cdce870644bc8f818
Evelin Gomez <gomez.evelin@hotmail.com>
2022-07-23 19:29:41 -0300

    count product terminado

---
+++ src/components/ItemCount/ItemCount.js
@@ -0,0 +1,23 @@
+ import './ItemCount.scss'
+ import {useState} from 'react'           
+            
+  const ItemCount = ({stock}) => {
+     let stockVirtual = stock
+     let disable= false
+     const [count, setCount] = useState(1)
+     const addProduct = () =>{
+        count <=stockVirtual ? setCount(count+1) : disable =true
+        }
+     const removeProduct = ()=>{
+       count > 1 ? setCount(count-1) : disable=true
+        }
+     return(   
+          <div className='itemCount'>
+               <button onClick={removeProduct} disabled={disable}>-</button>
+               <p>{count}</p>
+               <button onClick={addProduct} disabled={disable}>+</button>
+          </div>
+     )
+   }         
+        
+export default ItemCount
--- src/components/ItemProduct/ItemProduct.js
+++ src/components/ItemProduct/ItemProduct.js
@@ -1,26 +1,18 @@
 import './ItemProduct.scss'
-import {useState} from 'react'
+import ItemCount from '../ItemCount/ItemCount'
+
 
 const ItemProduct = ({data}) => {
-    const [count, setCount] = useState(1)
+    
     const {title,price,img,stock} = data
-    const addNumber = () =>{
-        setCount(count+1)
-    }
-    const removeNumber = ()=>{
-        setCount(count-1)
-    }
+    
     return(
         <div className='itemProduct'>
             <img className='imgProduct' src={img} alt="Imagen Producto"/>
             <p>{title}</p>
             <span>${price}</span>
-            <div className='itemCount'>
-                <button onClick={removeNumber}>-</button>
-                <p>{count}</p>
-                <button onClick={addNumber}>+</button>
-            </div>
-            <button>Comprar</button>
+            <ItemCount stock={stock}/>
+            <button>Agregar Carrito</button>
         </div>
     )  
 }
