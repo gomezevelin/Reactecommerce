084a7fd25892676f5e74889233ccbabec119d313
Evelin Gomez <gomez.evelin@hotmail.com>
2022-08-16 12:13:12 -0300

    estilos nav

--- src/components/NavBar/CartWidget.js
+++ src/components/NavBar/CartWidget.js
@@ -1,10 +1,10 @@
-
+import "./CartWidget.scss"
 import ShowCart from '../ShowCart/ShowCart'
 
 
 const CardWidget = ({contador}) => {
     return (
-        <div>
+        <div className='divCartWidget'>
             
             <ShowCart contador={contador}/>
         </div>
